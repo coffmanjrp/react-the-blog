fb620e48cda351442d941c75e36ad84053de9fa3
coffmanjrp <coffmanjrp@gmail.com>
2021-03-11 21:41:14 -0800

    Add contents to blog details page

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -1,12 +1,22 @@
 import { useParams } from 'react-router-dom';
+import useFetch from '../hooks/useFetch';
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const { data: blog, isPending, error } = useFetch(`/blogs/${id}`);
 
   return (
     <>
       <div className="blog-details">
-        <h2>Blog details - {id}</h2>
+        {isPending && <h2>Loading...</h2>}
+        {error && <h2>{error}</h2>}
+        {blog && (
+          <article>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+            <div>{blog.body}</div>
+          </article>
+        )}
       </div>
     </>
   );
